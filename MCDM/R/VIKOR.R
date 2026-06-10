8a6eaf11e4cd695f6ec4c5e468385b562f676549
Mohammadhadi Shahhosseini <53118758+mohmmadhadi@users.noreply.github.com>
2026-06-10 12:03:40 +0200

    Revert "Prepare MCDM package for CRAN publication"

--- MCDM/R/VIKOR.R
+++ MCDM/R/VIKOR.R
@@ -15,10 +15,10 @@
 #' weights <- c(0.4, 0.6)
 #' direction <- c("min", "max")
 #' v <- 0.5
-#' VIKOR(decision_matrix, weights, direction, v)
+#' vikor_ranking(decision_matrix, weights, direction, v)
 #'
 #' @export
-VIKOR <- function(matrix, weights, q, v) {
+vikor <- function(matrix, weights, q, v) {
   # Calculate the worst and best values for each criterion
   worst <- apply(matrix, 2, function(x) ifelse(q == "min", min(x), max(x)))
   best <- apply(matrix, 2, function(x) ifelse(q == "min", max(x), min(x)))
